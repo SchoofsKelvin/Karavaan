6ef2ee74418ac5c04ad39372635ec9151b5fdf33
Kelvin Schoofs <schoofs.kelvin@gmail.com>
2018-03-09 20:35:26 +0100

    Fix NewCurrencyPrompt and add visual feedback for empty fields

--- js/karavaan/prompt/newcurrency.js
+++ js/karavaan/prompt/newcurrency.js
@@ -10,12 +10,15 @@ import {
 
 import Prompt from './prompt';
 
+const STYLE_RED = { color: 'red' };
+
 class NewCurrencyPrompt extends Prompt {
   constructor(props) {
     super(props);
     this.props.onSubmit = () => this.close();
     this.props.onCancel = () => this.close();
     this.state = { tag: '', name: '' };
+    this.input_tag = '';
   }
   componentDidMount() {
     if (this.props.onRef) this.props.onRef(this);
@@ -24,21 +27,23 @@ class NewCurrencyPrompt extends Prompt {
     if (this.props.onRef) this.props.onRef(undefined);
   }
   get tag() {
-    return this.state.tag;
+    return this.state.tag.toUpperCase();
   }
   get name() {
-    return this.state.name;
+    return this.state.name.trim();
   }
   renderContent() {
+    const tagStyle = this.tag ? {} : STYLE_RED;
+    const nameStyle = this.name ? {} : STYLE_RED;
     return (
       <Form>
-        <Item floatingLabel>
+        <Item floatingLabel error={tagStyle == STYLE_RED}>
           <Label>Tag</Label>
-          <Input value={this.tag} uppercase onChangeText={v => this.setState(s => ({ ...s, tag: v && v.toUpperCase() }))} autoFocus />
+          <Input value={this.state.tag} onChangeText={tag => this.setState({ tag })} autoFocus />
         </Item>
-        <Item floatingLabel>
+        <Item floatingLabel error={nameStyle == STYLE_RED}>
           <Label>Name</Label>
-          <Input value={this.name} onChangeText={v => this.setState(s => ({ ...s, name: v }))} />
+          <Input value={this.state.name} onChangeText={name => this.setState({ name })} />
         </Item>
       </Form>);
   }
